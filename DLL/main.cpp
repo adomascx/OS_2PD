9b83aac500b5344bf77c34de875b1c968d1a3525
adomascx <adlukosev2@gmail.com>
2026-03-25 05:35:02 +0200

    Initial commit

---
+++ DLL/main.cpp
@@ -0,0 +1,7 @@
+#include <main.hpp>
+
+int main(int argc, char const *argv[])
+{
+    cout << TEXT;
+    return 0;
+}
---
+++ DLL/main.hpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+#define TEXT "Hello World!"
+
+using namespace std;
